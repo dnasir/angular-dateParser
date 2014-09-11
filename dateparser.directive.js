cfa8e38a0f1d0f5279991123b2dfc6aa3004124c
Robert Mulder <mulder@creativeid.nl>
2014-09-11 16:34:07 +0200

    Spaces instead of tabs at the request of Dzulqarnain Nasir

--- dateparser.directive.js
+++ dateparser.directive.js
@@ -1,35 +1,35 @@
 angular.module('dateParserDirective', ['dateParser'])
-	.directive('dateParser', ['dateFilter', '$dateParser', function(dateFilter, $dateParser) {
-		return {
-			restrict: 'A',
-			require: 'ngModel',
-			link: function(scope, element, attrs, ngModel) {
-				var dateFormat;
+    .directive('dateParser', ['dateFilter', '$dateParser', function(dateFilter, $dateParser) {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, element, attrs, ngModel) {
+                var dateFormat;
+                // If a new format is provided, update the view by rendering the model again.
+                attrs.$observe('dateParser', function(value) {
+                    dateFormat = value;
+                    // Leave the actual rendering to other directives. Angular provides these by default for <input>, <textarea> and <select>
+					if (angular.isFunction(ngModel.$render))
+                        ngModel.$render();
+                });
 
-				// If a new format is provided, update the view by rendering the model again.
-				attrs.$observe('dateParser', function(value) {
-					dateFormat = value;
-					// Leave the actual rendering to other directives. Angular provides these by default for <input>, <textarea> and <select>
-					ngModel.$render();
-				});
+                // Parse the input value to a date
+                ngModel.$parsers.push(function(viewValue) {
+                    var date = $dateParser(viewValue, dateFormat);
 
-				// Parse the input value to a date
-				ngModel.$parsers.push(function(viewValue) {
-					var date = $dateParser(viewValue, dateFormat);
+                    if (isNaN(date)) {
+                        ngModel.$setValidity('date', false);
+                    } else {
+                        ngModel.$setValidity('date', true);
+                    }
 
-					if (isNaN(date)) {
-						ngModel.$setValidity('date', false);
-					} else {
-						ngModel.$setValidity('date', true);
-					}
+                    return date;
+                });
 
-					return date;
-				});
-
-				// Format the new model value before it is displayed in the editor
-				ngModel.$formatters.push(function(modelValue) {
-					return modelValue ? dateFilter(modelValue, dateFormat) : '';
-				});
-			}
-		};
-	}]);
+                // Format the new model value before it is displayed in the editor
+                ngModel.$formatters.push(function(modelValue) {
+                    return modelValue ? dateFilter(modelValue, dateFormat) : '';
+                });
+            }
+        };
+    }]);
