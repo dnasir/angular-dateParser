951135b634cf7c48264f988546a3a5ac856bc587
Dzulqarnain Nasir <dzul1983@gmail.com>
2014-09-24 09:56:36 +0300

    * Changes broke unit test. Reverting to previous version.

--- dateparser.directive.js
+++ dateparser.directive.js
@@ -5,30 +5,35 @@ angular.module('dateParserDirective', ['dateParser'])
             require: 'ngModel',
             link: function(scope, element, attrs, ngModel) {
                 var dateFormat;
-                // If a new format is provided, update the view by rendering the model again.
+                
                 attrs.$observe('dateParser', function(value) {
                     dateFormat = value;
-                    // Leave the actual rendering to other directives. Angular provides these by default for <input>, <textarea> and <select>
-					if (angular.isFunction(ngModel.$render))
-                        ngModel.$render();
+                    ngModel.$render();
                 });
 
-                // Parse the input value to a date
-                ngModel.$parsers.push(function(viewValue) {
+                var parseDate = function(viewValue) {
                     var date = $dateParser(viewValue, dateFormat);
 
-                    if (isNaN(date)) {
+                    if(isNaN(date)) {
                         ngModel.$setValidity('date', false);
                     } else {
                         ngModel.$setValidity('date', true);
                     }
 
                     return date;
-                });
+                };
+                ngModel.$parsers.unshift(parseDate);
 
-                // Format the new model value before it is displayed in the editor
-                ngModel.$formatters.push(function(modelValue) {
-                    return modelValue ? dateFilter(modelValue, dateFormat) : '';
+                ngModel.$render = function() {
+                    var date =  ngModel.$modelValue ? dateFilter(ngModel.$modelValue, dateFormat) : '';
+                    element.val(date);
+                    scope.ngModel = ngModel.$modelValue;
+                };
+
+                element.bind('input change keyup', function() {
+                    scope.$apply(function() {
+                        scope.ngModel = ngModel.$modelValue;
+                    });
                 });
             }
         };
